4ce164d5e5eb59d5cf7bc5c0d480ec543d9e9380
Kelvin <kelvinnicolau7@gmail.com>
2022-08-02 14:36:17 -0300

    commitando missao 5

---
+++ js/index.js
@@ -0,0 +1,47 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, child, get, set } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "exemplo",
+  authDomain: "exemplo.firebaseapp.com",
+  databaseURL: "https://exemplo.firebaseio.com",
+  projectId: "exemplo",
+  storageBucket: "exemplo.appspot.com",
+  messagingSenderId: "1234",
+  appId: "1234"
+};
+
+const app = initializeApp(firebaseConfig);
+
+var APP = {
+  insert: function (key, id, data) {
+    const db = getDatabase(app);
+    set(ref(db, 'key/' + id), data);
+  },
+  read: function () {
+    const dbRef = ref(getDatabase(app));
+    get(child(dbRef, `matches`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        var dados = snapshot.val();
+        var resultado = APP.filtrarLista(dados, "Senegal");
+        console.log(resultado);
+      } else {
+        console.log("Não há dados para serem exibidos");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  },
+
+  filtrarLista: function (lista, termo) {
+    return lista.filter(function (item) {
+      return item.team1 == termo || item.team2 == termo;
+    });
+  },
+
+  init: function () {
+    APP.read();
+  }
+}
+
+APP.init();
